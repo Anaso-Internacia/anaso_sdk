fa2065de3b222ec241cd8b350a0714a31fba743b
Brandon Dyer <brandondyer64@gmail.com>
2023-11-21 17:09:26 -0600

    ReqPostList

--- anaso_api_models/src/request/mod.rs
+++ anaso_api_models/src/request/mod.rs
@@ -1,9 +1,10 @@
 use std::borrow::Cow;
 
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_email::Email;
 
-use crate::id::AnasetoId;
+use crate::id::{AnasetoId, UserId};
 
 #[derive(Deserialize, Serialize)]
 pub struct ReqRegisterUser<'a> {
@@ -29,4 +30,30 @@ pub struct ReqNewPost<'a> {
     pub anaseto_id: AnasetoId,
     pub title: Cow<'a, str>,
     pub body_md: Cow<'a, str>,
+}
+
+/// How to sort posts
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub enum PostSort {
+    /// Return posts sorted from newest to oldest
+    New,
+
+    /// Return posts sorted by how well they are trending
+    #[default]
+    Hot,
+
+    /// Return posts sorted purely by score
+    Top {
+        /// Box the posts returned to a particular time span
+        ///
+        /// If [`None`], will include all posts from all time
+        since: Option<DateTime<Utc>>,
+    },
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct ReqListPosts {
+    pub anaseto: Option<AnasetoId>,
+    pub user: Option<UserId>,
+    pub sort: PostSort,
 }
